edcd36ed7e001ba1aae2b1d45d9380334dda9bcf
isa-bolanos <isa.bolanos55@gmail.com>
2023-05-05 21:34:51 -0500

    Initial Commit

---
+++ real assignment notebook/real_assignment_notebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_assignment_notebookApp.swift
+//  real assignment notebook
+//
+//  Created by isa bolanos on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct real_assignment_notebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
